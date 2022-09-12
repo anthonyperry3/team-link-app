59caa9302981ba10f8fde11a8d3d680c89cc9844
Sergutt <sergut_t@yahoo.com>
2022-09-12 18:58:24 -0400

    added firebase

---
+++ client/Firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPAze9R2iDc_5k_b2IVjhqTI1z7FTe2go",
+  authDomain: "team-link-app.firebaseapp.com",
+  projectId: "team-link-app",
+  storageBucket: "team-link-app.appspot.com",
+  messagingSenderId: "950670369478",
+  appId: "1:950670369478:web:bed0366cddb27667f5adc7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
