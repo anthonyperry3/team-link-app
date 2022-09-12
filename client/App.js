899bdc328d67fa77a3b0942194f51456093c7400
Anthony <aperryiii20@gmail.com>
2022-09-12 14:41:06 -0700

    screen stack added

--- client/App.js
+++ client/App.js
@@ -1,14 +1,56 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+import LandingPage from "./screens/LandingPage";
+import LoginPage from "./screens/LoginPage";
+import ProfilePage from "./screens/ProfilePage";
+
+import firebase from "./Firebase/firebase";
+// import { getAuth, onAuthStateChanged } from "./Firebase/firebase";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function HomeTabs() {
+
   return (
-    <View style={styles.container}>
-      <Text>Jesse Branch</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Finder" component={Finder} />
+      <Tab.Screen name="Matches" component={Matches} />
+    </Tab.Navigator>
   );
 }
+
+function App() {
+
+  const userAuth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(userAuth, (user) => {
+      if (user !== null) {
+        setUserId(user.uid);
+      } else {
+        setUserId("");
+      }
+    });
+  }, []);
+
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName = "Login" screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Landing" component={LandingPage} />
+      <Stack.Screen name="Login" component={LoginPage} />
+      <Stack.Screen name="Home" component={HomeTabs} />
+    </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ client/screens/LandingPage.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './LandingPageStyles'
+
+const LandingPage = () => {
+  return (
+    <View>
+      <Text>Join</Text>
+    </View>
+  )
+}
+
+export default LandingPage;
