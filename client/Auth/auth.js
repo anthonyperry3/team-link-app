ce652cf0d617dbda2244f2fe0f8fcfab4b499558
Sergutt <sergut_t@yahoo.com>
2022-09-13 13:01:56 -0400

    first commit

--- client/Auth/auth.js
+++ client/Auth/auth.js
@@ -1,67 +1,12 @@
-// import { View, Text } from 'react-native'
-// import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-// const auth = () => {
-//   return (
-//     <View>
-//       <Text>auth</Text>
-//     </View>
-//   )
-// }
-
-// export default auth;
-
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, Button, View, TextInput } from "react-native";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-
-const Auth = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const register = () => {
-    createUserWithEmailAndPassword(props.userAuth, email, password);
-  };
-
-  const login = () => {
-    signInWithEmailAndPassword(props.userAuth, email, password);
-  };
-
-  useEffect(() => {
-    console.log("the thing ====>", props.userId);
-    if (props.userId !== "") {
-      props.navigation.navigate("Home");
-    }
-  }, [props.userId]);
-
+const auth = () => {
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.input} onChangeText={setEmail} value={email} />
-      <TextInput
-        style={styles.input}
-        onChangeText={setPassword}
-        value={password}
-      />
-      <Button onPress={register} title="Register" />
-      <Button onPress={login} title="Login" />
+    <View>
+      <Text>auth</Text>
     </View>
   );
 };
 
-export default Auth;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  input: {
-    borderWidth: 1,
-  },
-});
+export default auth;
