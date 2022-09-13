bf4a2362ad0476de73715850f15719ed4a7a505f
Sergutt <sergut_t@yahoo.com>
2022-09-13 19:47:54 -0400

    import home

--- client/App.js
+++ client/App.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import LandingPage from "./screens/LandingPage";
 import LoginPage from "./screens/LoginPage";
 import ProfilePage from "./screens/ProfilePage";
-
+import Home from "./screens/Home";
 import firebase from "./Firebase/firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
