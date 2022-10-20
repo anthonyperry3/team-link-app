050ba02e693f722441f3280602bcc8f9282b4676
Angel <abarranco650@gmail.com>
2022-10-20 10:51:53 -0700

    cleaned commented out code

--- client/screens/ProfilePageStyles.js
+++ client/screens/ProfilePageStyles.js
@@ -3,10 +3,8 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // margin: 30,
     padding: 30,
-    // backgroundColor: "#fff",
-    // justifyContent: "space-between",
+ 
   },
   pageTitle: {
     fontSize: 36,
